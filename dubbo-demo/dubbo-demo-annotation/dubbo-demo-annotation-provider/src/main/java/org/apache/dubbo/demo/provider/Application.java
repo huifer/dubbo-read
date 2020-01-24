e2ae849a7b5c225729e1debe273726464844ac37
huifer <huifer97@163.com>
2020-01-24 09:32:24 +0800

    环境搭建

--- dubbo-demo/dubbo-demo-annotation/dubbo-demo-annotation-provider/src/main/java/org/apache/dubbo/demo/provider/Application.java
+++ dubbo-demo/dubbo-demo-annotation/dubbo-demo-annotation-provider/src/main/java/org/apache/dubbo/demo/provider/Application.java
@@ -40,7 +40,7 @@ public class Application {
         @Bean
         public RegistryConfig registryConfig() {
             RegistryConfig registryConfig = new RegistryConfig();
-            registryConfig.setAddress("zookeeper://127.0.0.1:2181");
+            registryConfig.setAddress("zookeeper://127.0.0.1:32771");
             return registryConfig;
         }
     }
